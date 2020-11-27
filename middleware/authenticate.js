8d41cb444717c4399b7775017c2df5fbdbf1e515
Hai <vanhai.qtv@gmail.com>
2020-11-27 23:51:42 +0700

    just test for deploy

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,12 +3,13 @@ const sessionModel = require("../model/mongooseModel/sessionModel");
 
 exports.authenUser = async (req, res, next) => {
   {
+    console.log("sess ", req.session);
     if (!req.session || !req.session.id) {
       console.log("da log");
       res.redirect("/login");
       return;
     }
-
+    console.log("sessID ", req.sessionID);
     const session = await sessionModel.find({ _id: req.sessionID });
     console.log("kich thuoc ", session.length, "  ", session);
     if (session.length > 0) {
