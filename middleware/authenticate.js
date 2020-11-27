df16cb0365aea8d0f68bde8a7e160f400760a4b6
Hai <vanhai.qtv@gmail.com>
2020-11-27 23:36:55 +0700

    test session for deploy

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,13 +3,14 @@ const sessionModel = require("../model/mongooseModel/sessionModel");
 exports.authenUser = async (req, res, next) => {
   {
     if (!req.session || !req.session.id) {
+      console.log("da log");
       res.redirect("/login");
       return;
     }
 
     let session = await sessionModel.find({ _id: req.session.id });
-
-    if (session[0]) {
+    console.log("kich thuoc ", session.length, "  ", session);
+    if (session.length > 0) {
       next();
     } else {
       res.redirect("/login");
