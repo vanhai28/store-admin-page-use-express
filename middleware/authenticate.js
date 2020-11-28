6bb56e048d9cbeb9da940189d6f84410ddc34b6c
Hai <vanhai.qtv@gmail.com>
2020-11-28 10:01:45 +0700

    test

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,14 +3,15 @@ const sessionModel = require("../model/mongooseModel/sessionModel");
 
 exports.authenUser = async (req, res, next) => {
   {
-    console.log("cookie ", req.session.cookie);
-    if (!req.session.cookie || !req.session.cookie.sessionID) {
+    let cookie = req.session.cookie;
+    console.log("cookie ", cookie);
+    if (!cookie || !cookie.sessionID) {
       console.log("da log");
       res.redirect("/login");
       return;
     }
-    console.log("sessID ", req.sessionID);
-    const session = await sessionModel.find({ _id: req.sessionID });
+    console.log("sessID ", cookie.sessionID);
+    const session = await sessionModel.find({ _id: cookie.sessionID });
     console.log("kich thuoc ", session.length, "  ", session);
     if (session.length > 0) {
       next();
